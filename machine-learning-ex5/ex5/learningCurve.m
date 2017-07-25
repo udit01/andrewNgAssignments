aeb24bcecf7b62e74a81670e6eab3926a37a1585
udit01 <uditjainiitd@gmail.com>
2017-07-25 19:03:44 +0530

    raw formula is not working but ...on calling the linearRegCostFunction it works... ex5

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -53,9 +53,15 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 for i = 1:m
-    theta = trainLinearReg( X(1:i,:) ,y(1:i) , 1 );
-    error_train(i) = (sum((X(1:i,:)*theta - y(1:i)).^2))/(2*m);
-    error_val(i) = (sum((Xval*theta - yval).^2))/(2*m);
+%    theta = zeros(size(X,2) ,1);
+    theta = trainLinearReg( X(1:i,:) ,y(1:i) ,lambda);
+%    theta = trainLinearReg( X,y, lambda );
+%     error_train(i) = (sum((X(1:i,:)*theta - y(1:i)).^2))/(2*m);
+    error_train(i) = linearRegCostFunction(X(1:i,:) , y(1:i) , theta , 0);
+%    error_train(i) = (sum((X*theta - y).^2))/(2*m);
+%     error_val(i) = (sum((Xval*theta - yval).^2))/(2*m);
+
+    error_val(i) = linearRegCostFunction(Xval , yval , theta , 0);
 end
 
 
